e277a28e04d79798389ae79fd83407d867b5b1c7
AmirAllami <allamiamir@gmail.com>
2020-04-16 13:39:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Buttons from "./componants/Buttons";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: "",
+    };
+    this.changingInput = this.changingInput.bind(this);
+  }
+  changingInput = (a) => {
+    let num2 = this.state.num + a;
+    this.setState({ num: num2 });
+  };
+  render() {
+    return (
+      <div className="App">
+        <input type="text" value={this.state.num} />
+        <Buttons p={this.changingInput} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componants/Buttons.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+class Buttons extends React.Component {
+  render(props) {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.props.p(1);
+          }}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(2);
+          }}
+        >
+          {" "}
+          2
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(3);
+          }}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(4);
+          }}
+        >
+          4
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(5);
+          }}
+        >
+          5
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(6);
+          }}
+        >
+          6
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(7);
+          }}
+        >
+          7
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(8);
+          }}
+        >
+          8
+        </button>
+        <button
+          onClick={() => {
+            this.props.p(9);
+          }}
+        >
+          9
+        </button>
+        <button>+</button>
+        <button>-</button>
+        <button>×</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    );
+  }
+}
+
+export default Buttons;

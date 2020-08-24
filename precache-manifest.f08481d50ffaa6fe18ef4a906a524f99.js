7298db6553e3721041c107ac8372593b429524f3
AmirAllami <allamiamir@gmail.com>
2020-08-24 19:45:01 +0100

    Updates

---
+++ precache-manifest.f08481d50ffaa6fe18ef4a906a524f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2b187ed8dd76744f77e31f6810f5e3",
+    "url": "/Calculator-/index.html"
+  },
+  {
+    "revision": "9917d5fe30426ce21ca2",
+    "url": "/Calculator-/static/css/main.cca61cde.chunk.css"
+  },
+  {
+    "revision": "f273bdc05d1319f9e0fe",
+    "url": "/Calculator-/static/js/2.e9d5f99d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-/static/js/2.e9d5f99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9917d5fe30426ce21ca2",
+    "url": "/Calculator-/static/js/main.d60925c8.chunk.js"
+  },
+  {
+    "revision": "b13074b876b5cc818c41",
+    "url": "/Calculator-/static/js/runtime-main.bd30ba26.js"
+  }
+]);
